1af1452fc4d6b961046172bc2a8154ae5cfc1644
Linhai_PC <13838886000@163.com>
2023-04-22 11:45:59 +0800

    20230422

--- 11、对象的初始化和清理/拷贝函数/拷贝函数的应用时机.cpp
+++ 11、对象的初始化和清理/拷贝函数/拷贝函数的应用时机.cpp
@@ -1,1 +1,36 @@
 # include <iostream>
+
+using namespace std;
+
+class Person
+{
+public:
+    Person ()
+    {
+        cout << "无参构造" << endl;
+        P_age = 0;
+    }
+
+    Person(int age)
+    {
+        cout << "有参构造" << endl;
+        P_age = age;
+    }
+
+    Person(const Person & P)
+    {
+        
+    }
+
+
+private:
+    int P_age;
+    int P_height;
+}
+
+int main()
+{
+
+    system("pause");
+    return 0;    
+}
