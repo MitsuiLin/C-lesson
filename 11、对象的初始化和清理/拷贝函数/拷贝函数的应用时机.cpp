43f12a2228ea4e71ce41d5403398fa8f0548e251
MitsuiLin <13838886000@163.com>
2023-04-24 12:13:58 +0800

    20230424

--- 11、对象的初始化和清理/拷贝函数/拷贝函数的应用时机.cpp
+++ 11、对象的初始化和清理/拷贝函数/拷贝函数的应用时机.cpp
@@ -1,36 +1,52 @@
 # include <iostream>
-
 using namespace std;
 
 class Person
 {
 public:
-    Person ()
+    Person()
     {
-        cout << "无参构造" << endl;
-        P_age = 0;
+        cout << "无参构造函数" << endl;
+        age = 0;
+        height = 0;
     }
 
-    Person(int age)
+    Person(int a, int b)
     {
-        cout << "有参构造" << endl;
-        P_age = age;
+        cout << "有参构造函数" << endl;
+        age = a;
+        height = b;
     }
 
     Person(const Person & P)
     {
-        
+        age = P.age;
+        height = P.height;
+        cout << "拷贝构造函数" << endl;
     }
 
+    ~Person()
+    {
+        cout << "析构函数" << endl;
+    }
 
-private:
-    int P_age;
-    int P_height;
+public:
+    int age;
+    int height;
+};
+
+void test01()
+{
+    Person p1(20, 50);
+    Person p2(p1);
+    cout << p1.age << p1.height << endl;
+    cout << p2.age << p2.height << endl;
 }
 
 int main()
 {
+    test01();
 
     system("pause");
-    return 0;    
+    return 0;
 }
